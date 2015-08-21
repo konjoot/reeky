b9ecfd98bc58ac3310451c19408d3597b0176f39
Maksimov Sergey <konjoot@gmail.com>
2015-08-21 20:02:29 +0300

    welcome Creator handler

---
+++ handlers/creator.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+
+	i "github.com/konjoot/reeky/interfaces"
+)
+
+func Creator(c *echo.Context) (e error) {
+	var r i.ResourceIface
+
+	if r, e = Resource(c); e != nil {
+		return
+	}
+
+	if e = c.Bind(r.Form()); e != nil {
+		return
+	}
+
+	if e = r.Save(); e != nil {
+		return
+	}
+
+	c.Response().Header().Set("Location", r.Url())
+
+	if e = c.JSON(http.StatusCreated, ""); e != nil {
+		return
+	}
+
+	return
+}
